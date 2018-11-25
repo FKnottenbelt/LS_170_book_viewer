e2338de3bc38c14d2425d7b0fc4af09afc0611a9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-11-25 09:53:52 +0000

    Add before filter

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,9 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
@@ -12,7 +15,6 @@ end
 get "/chapter/:number" do
   number = params[:number]
 
-  @contents = File.readlines("data/toc.txt")
   index = number.to_i - 1
   chapter_title = @contents[index]
 
