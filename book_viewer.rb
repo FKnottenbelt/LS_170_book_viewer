5c863ded5ee7bffc2b7c4c51c4a7b672cf241776
Femke Knottenbelt <femknotgithub@gmail.com>
2018-11-26 19:29:02 +0000

    Add paragraph to search

--- book_viewer.rb
+++ book_viewer.rb
@@ -8,21 +8,40 @@ end
 
 helpers do
   def in_paragraphs(chapter_content)
-    chapter_content.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    chapter_content.split("\n\n").map.with_index do |paragraph, indx|
+      "<p id=#{indx}>#{paragraph}</p>"
     end.join
+  end
+
+  def select_paragraphs(file, query)
+    selected = []
+
+    file.split("\n\n").map.with_index do |paragraph, indx|
+      selected << "<p id=#{indx}>#{paragraph}</p>" if paragraph.include?(query)
+    end
+    selected
+  end
+
+  def make_search_result_html(title_link, paragraphs)
+    paragraphs.map do |paragraph|
+        "#{title_link} #{paragraph}"
+    end
+  end
+
+  def get_title_link(file_name)
+    chapter_num = file_name.scan(/\d+/).join
+    title = @contents[chapter_num.to_i - 1]
+    "<li><a href='/chapter/#{chapter_num}'>#{title}</a></li>"
   end
 
   def search_files(file_names, query)
     file_names.map do |file_name|
       file = File.read("#{file_name}")
-
       next unless !!file.match(/#{query}/)
 
-      chapter_num = file_name.scan(/\d+/).join
-      title = @contents[chapter_num.to_i - 1]
-
-      "<li><a href='/chapter/#{chapter_num}'>#{title}</a></li>"
+      title_link = get_title_link(file_name)
+      paragraphs = select_paragraphs(file, query)
+      make_search_result_html(title_link, paragraphs)
     end.join
   end
 
